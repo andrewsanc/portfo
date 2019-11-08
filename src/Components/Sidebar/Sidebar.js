554ba7379e6cb50d41d6d973ae459317fbaab9f0
TB831 <druu831@gmail.com>
2019-11-07 17:39:53 -0800

    changed googleDrive download

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './sidebar.css';
-import me from '../../static/me.jpg'
+import React from "react";
+import "./sidebar.css";
+import me from "../../static/me.jpg";
 
 const Sidebar = () => {
   return (
@@ -9,16 +9,33 @@ const Sidebar = () => {
         <img src={me} alt="me" />
         <h1>Andrew Sanchez</h1>
         <h2>Software Engineer</h2>
-        <a href="https://drive.google.com/uc?export=download&id=1Um0lm2njWP6qgT46T24mmOD-w95WKL3G" className="btn-rounded-white">Download Resume</a>
+        <a
+          href="https://drive.google.com/uc?export=download&id=1vf4npHAZn_8TiMlp0N3JgGVM1ycTmIfi"
+          className="btn-rounded-white"
+        >
+          Download Resume
+        </a>
         <ul className="social">
-          <li><a href="https://www.linkedin.com/in/andrewsanc"><i className="fab fa-linkedin"></i></a></li>
-          <li><a href="https://github.com/andrewsanc"><i className="fab fa-github"></i></a></li>
-          <li><a href="https://instagram.com/druusanchez"><i className="fab fa-instagram"></i></a></li>
+          <li>
+            <a href="https://www.linkedin.com/in/andrewsanc">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/andrewsanc">
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://instagram.com/druusanchez">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </li>
         </ul>
       </div>
       <div className="sidebar-overlay"></div>
     </div>
   );
-}
- 
+};
+
 export default Sidebar;
