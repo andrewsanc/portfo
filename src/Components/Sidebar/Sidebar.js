8f2f3bd2573073af3f04c71b056ac42bf26f8444
TB831 <druu831@gmail.com>
2021-07-26 16:35:23 -0700

    new resume download link

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,31 +4,31 @@ import me from "../../static/me.jpg";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <div className="sidebar-content">
-        <img src={me} alt="me" />
+    <div className='sidebar'>
+      <div className='sidebar-content'>
+        <img src={me} alt='me' />
         <h1>Andrew Sanchez</h1>
         <h2>Software Engineer</h2>
         <a
-          href="https://drive.google.com/uc?export=download&id=1vf4npHAZn_8TiMlp0N3JgGVM1ycTmIfi"
-          className="btn-rounded-white"
+          href='https://drive.google.com/uc?export=download&id=1G70UMwvaNjPL0BzXFv8NycR_E_xGVXlL'
+          className='btn-rounded-white'
         >
           Download Resume
         </a>
-        <ul className="social">
+        <ul className='social'>
           <li>
-            <a href="https://www.linkedin.com/in/andrewsanc">
-              <i className="fab fa-linkedin"></i>
+            <a href='https://www.linkedin.com/in/andrewsanc'>
+              <i className='fab fa-linkedin'></i>
             </a>
           </li>
           <li>
-            <a href="https://github.com/andrewsanc">
-              <i className="fab fa-github"></i>
+            <a href='https://github.com/andrewsanc'>
+              <i className='fab fa-github'></i>
             </a>
           </li>
           <li>
-            <a href="https://instagram.com/druusanchez">
-              <i className="fab fa-instagram"></i>
+            <a href='https://instagram.com/druusanchez'>
+              <i className='fab fa-instagram'></i>
             </a>
           </li>
         </ul>
