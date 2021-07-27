6fbea8adaaabea245db7b1c34eaacd7f9f8f32b1
TB831 <druu831@gmail.com>
2021-07-27 10:29:09 -0700

    converted project blocks into project cards

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -7,8 +7,12 @@ const projects = resumeInfo.projects;
 const Projects = () => {
   return (
     <div className='projects'>
-      <h2 className='heading'>Projects</h2>
-      {renderProjects(projects)}
+      <div className='container'>
+        <div className='col-sm-12'>
+          <h2 className='heading'>Projects</h2>
+          <div className='row row-evenly'>{renderProjectCards(projects)}</div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -23,6 +27,28 @@ const renderProjects = (projects) => {
         <div className='project-info'>
           <h3>{project.title}</h3>
           <div className='project-tags'>{renderProjectTags(project.tags)}</div>
+          <a href={project.repository}>View Project</a>
+          {project.deployment && (
+            <a href={project.deployment}>View Deployment</a>
+          )}
+        </div>
+      </div>
+    );
+  });
+};
+
+const renderProjectCards = (projects) => {
+  return projects.map((project, i) => {
+    return (
+      <div className='project-card shadow-large'>
+        <h5 className='project-card-header'>{project.title}</h5>
+        <div className='project-card-image'>
+          <img src={project.projectImage} alt={project.imageAlt} />
+        </div>
+        <div className='project-card-container'>
+          {renderProjectTags(project.tags)}
+        </div>
+        <div className='project-card-container-bottom'>
           <a href={project.repository}>View Project</a>
           {project.deployment && (
             <a href={project.deployment}>View Deployment</a>
